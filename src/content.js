1df89145616d3afce6c84683af677ae33fa9459c
Valery Vitko <valery.vitko@gmail.com>
2016-01-19 11:57:32 +0300

    Quick solution for JIRA links highlight

--- src/content.js
+++ src/content.js
@@ -7,12 +7,22 @@ function onLinksAdded(links) {
 };
 
 function buildLink(element) {
-	var issueNumber = element.innerText.match(/\d{8,9}\b/);
-	if (issueNumber) {
+	var jiraIssueKey = element.innerText.match(/\b[A-Z]{2}-\d{3,5}\b/);
+	if (jiraIssueKey) {
+		element.innerHTML = element.innerHTML.replace(
+			/\b([A-Z]{2}-\d{3,5})\b/,
+			'<a target="_blank" onclick="event.stopPropagation()" href="https://socialwellth.atlassian.net/browse/$1">$1</a>'
+		);
+		return;
+	}
+
+	var ptStoryNumber = element.innerText.match(/\d{8,9}\b/);
+	if (ptStoryNumber) {
 		element.innerHTML = element.innerHTML.replace(
 			/#?(\d{8,9})\b/,
 			'<a target="_blank" onclick="event.stopPropagation()" href="https://www.pivotaltracker.com/story/show/$1">#$1</a>'
 		);
+		return;
 	}
 }
 
